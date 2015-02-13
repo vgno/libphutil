1df64ef606c7620fa0e51e57347aee26463da5a6
Erland Wiencke <erland.wiencke@vg.no>
2015-02-13 14:57:10 +0100

    Added deployer to the libphutil project

---
+++ deploy.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'recipe/common.php';
+
+server('development-server', 'phabricator-dev.int.vgnett.no', 22)
+    ->path('/services/applications/phabricator-dev/libphutil')
+    ->user('erlwienc')
+    ->pubKey();
+
+stage('dev', array('development-server'), array('branch' => 'master'), true);
+
+set('repository', 'git@github.com:vgno/libphutil.git');
+
+task('deploy', [
+    'deploy:start',
+    'deploy:prepare',
+    'deploy:update_code',
+    'deploy:symlink',
+    'cleanup',
+    'deploy:end'
+])->desc('Deploy your project');
